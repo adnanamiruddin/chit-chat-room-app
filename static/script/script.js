009b0ec9ca3ddb63daed687fe96892f2ca15dacc
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-06-09 13:22:52 +0800

    fix: Delete JS code in room.html and move it to script.js

--- static/script/script.js
+++ static/script/script.js
@@ -1,37 +1,51 @@
 const socketio = io();
 
-const messages = document.getElementById("messages");
+  const messages = document.getElementById("messages");
 
-const createMessage = (name, msg) => {
-  const content = `
+  const createMessage = (name, msg) => {
+    const content = `
     <div class="flex flex-row justify-between items-center px-2 pt-4 overflow-hidden text-sm">
-      <span class="w-4/6  break-words md:w-9/12">
-        <strong>${name}</strong>: ${msg}
-      </span>
+      <div class="w-4/6  break-words md:w-9/12">
+        <div style="font-size: 15px;">
+          <strong>${name}</strong>
+        </div>
+        <div style="font-size: 20px;">
+          ${msg}
+        </div>
+      </div>
       <span class="text-xs text-gray-500 w-1/6">
         ${new Date().toLocaleString()}
       </span>
     </div>
   `;
-  messages.innerHTML += content;
-  scrollToBottom();
-};
+    messages.innerHTML += content;
+    scrollToBottom();
+  };
 
-socketio.on("message", (data) => {
-  createMessage(data.name, data.message);
-});
+  socketio.on("message", (data) => {
+    createMessage(data.name, data.message);
+  });
 
-const sendMessage = (e) => {
-  e.preventDefault();
-  const message = document.getElementById("message");
-  if (message.value === "" || message.value === " ") return;
-  socketio.emit("message", { data: message.value });
-  message.value = "";
-  setTimeout(scrollToBottom, 100); // Delay to ensure message is rendered before scrolling
-};
+  const sendMessage = (e) => {
+    e.preventDefault();
+    const message = document.getElementById("message");
+    if (message.value === "" || message.value === " ") return;
+    socketio.emit("message", { data: message.value });
+    message.value = "";
+    setTimeout(scrollToBottom, 100); // Delay to ensure message is rendered before scrolling
+  };
 
-const scrollToBottom = () => {
-  messages.scrollTo({ top: messages.scrollHeight, behavior: "smooth" });
-};
+  const scrollToBottom = () => {
+    messages.scrollTo({ top: messages.scrollHeight, behavior: "smooth" });
+  };
 
-window.addEventListener("load", scrollToBottom);
+  window.addEventListener("load", scrollToBottom);
+
+  messages.addEventListener("scroll", () => {
+    const scrollOffset = messages.scrollHeight - messages.scrollTop;
+    const containerHeight = messages.clientHeight;
+    const scrollDifference = scrollOffset - containerHeight;
+    if (scrollDifference < 5) {
+      scrollToBottom();
+    }
+  });

5b9fcf84de6ad7a747103e11993168350edef306
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-06-09 13:38:15 +0800

    fix: Edit container date and hour in script.js

--- static/script/script.js
+++ static/script/script.js
@@ -1,51 +1,51 @@
 const socketio = io();
 
-  const messages = document.getElementById("messages");
+const messages = document.getElementById("messages");
 
-  const createMessage = (name, msg) => {
-    const content = `
-    <div class="flex flex-row justify-between items-center px-2 pt-4 overflow-hidden text-sm">
+const createMessage = (name, msg) => {
+  const content = `
+    <div class="flex flex-row justify-between items-center px-2 pt-4 overflow-hidden text-sm md:px-6">
       <div class="w-4/6  break-words md:w-9/12">
-        <div style="font-size: 15px;">
+        <div class="text-sm">
           <strong>${name}</strong>
         </div>
-        <div style="font-size: 20px;">
+        <div class="text-lg">
           ${msg}
         </div>
       </div>
-      <span class="text-xs text-gray-500 w-1/6">
+      <span class="text-xs text-gray-500 w-1/6 text-end">
         ${new Date().toLocaleString()}
       </span>
     </div>
   `;
-    messages.innerHTML += content;
-    scrollToBottom();
-  };
+  messages.innerHTML += content;
+  scrollToBottom();
+};
 
-  socketio.on("message", (data) => {
-    createMessage(data.name, data.message);
-  });
+socketio.on("message", (data) => {
+  createMessage(data.name, data.message);
+});
 
-  const sendMessage = (e) => {
-    e.preventDefault();
-    const message = document.getElementById("message");
-    if (message.value === "" || message.value === " ") return;
-    socketio.emit("message", { data: message.value });
-    message.value = "";
-    setTimeout(scrollToBottom, 100); // Delay to ensure message is rendered before scrolling
-  };
+const sendMessage = (e) => {
+  e.preventDefault();
+  const message = document.getElementById("message");
+  if (message.value === "" || message.value === " ") return;
+  socketio.emit("message", { data: message.value });
+  message.value = "";
+  setTimeout(scrollToBottom, 100); // Delay to ensure message is rendered before scrolling
+};
 
-  const scrollToBottom = () => {
-    messages.scrollTo({ top: messages.scrollHeight, behavior: "smooth" });
-  };
+const scrollToBottom = () => {
+  messages.scrollTo({ top: messages.scrollHeight, behavior: "smooth" });
+};
 
-  window.addEventListener("load", scrollToBottom);
+window.addEventListener("load", scrollToBottom);
 
-  messages.addEventListener("scroll", () => {
-    const scrollOffset = messages.scrollHeight - messages.scrollTop;
-    const containerHeight = messages.clientHeight;
-    const scrollDifference = scrollOffset - containerHeight;
-    if (scrollDifference < 5) {
-      scrollToBottom();
-    }
-  });
+// messages.addEventListener("scroll", () => {
+//   const scrollOffset = messages.scrollHeight - messages.scrollTop;
+//   const containerHeight = messages.clientHeight;
+//   const scrollDifference = scrollOffset - containerHeight;
+//   if (scrollDifference < 5) {
+//     scrollToBottom();
+//   }
+// });

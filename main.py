5c1083662855c70c886816f71d4c9a7edacec225
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-06-12 22:34:51 +0800

    fix: Change Home page's text title

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 from string import ascii_uppercase
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "haushaa"
+app.config["SECRET_KEY"] = "kelompok2"
 socketio = SocketIO(app)
 
 rooms = {}
